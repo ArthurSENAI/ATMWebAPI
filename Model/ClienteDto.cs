4124feb8015f72e4c4ccc274cf2e99106b11415a
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-10-02 17:50:13 -0300

    Initial commit

---
+++ Model/ClienteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ATMWebAPI.Model
+{
+    public class ClienteDto
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public IFormFile Documento { get; set; }
+    }
+}
